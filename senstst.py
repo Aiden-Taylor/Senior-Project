e4faafe0b4d684e7b251e6201832a6b4e512b753
Aiden-Taylor <magootaylor@gmail.com>
2024-02-29 11:14:36 -0800

    Update senstst.py

--- senstst.py
+++ senstst.py
@@ -6,6 +6,8 @@ lsm = gyroobj.gyroobj()
 mmc = compass.compass()
 
 while True:
-    print(lsm.getAngle())
-    print(mmc.getCompass())
+    mag_x, mag_y, mag_z = mmc.getCompass()
+    
+    print("X:{0:10.2f}, Y:{1:10.2f}, Z:{2:10.2f}".format(mag_x, mag_y, mag_z))
+    #print(lsm.getAngle())
     time.sleep(1)
