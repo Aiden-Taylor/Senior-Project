f3c0c5408591bbfcc24772142317f99e31d86992
Aiden-Taylor <magootaylor@gmail.com>
2024-03-05 10:36:12 -0800

    Changes to be committed: 	modified:   senstst.py

--- senstst.py
+++ senstst.py
@@ -9,5 +9,5 @@ while True:
     mag_x, mag_y, mag_z = mmc.getCompass()
     
     print("X:{0:10.2f}, Y:{1:10.2f}, Z:{2:10.2f}".format(mag_x, mag_y, mag_z))
-    #print(lsm.getAngle())
+    print(lsm.getAngle())
     time.sleep(1)
