a010a8e6cd3f07cba5a8609fef69f8a5c9f3d1e2
Aiden-Taylor <magootaylor@gmail.com>
2024-03-05 22:42:04 -0800

    Add angle

--- gyroobj.py
+++ gyroobj.py
@@ -20,11 +20,15 @@ class gyroobj:
         self.zenith = math.degrees(math.atan(y/x))
         #1/(1/(atan(z/(x^2 + y^2)^(1/2))^2 + atan(y/x)^2))^(1/2)
         self.elevation = 90 - math.degrees(1/math.sqrt(1/(pow(math.atan(z/(math.sqrt(pow(x,2) + pow(y,2)))), 2) + pow(math.atan(y/x), 2))))
-        angle = self.elevation
-        return(angle)
+        return(self.elevation)
     
+    def getCurrAngle(self):
+        x, y, z = self.accelero.acceleration
+        self.theta = math.asin(math.atan(y/x)/self.getHillAngle())
+        return(self.theta)
+
     def getCurrAzimuth(self):
 
-
+        
         angle = self.accelero.acceleration
         return(angle)
