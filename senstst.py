8ebd03028261de369e03214163481442751b55f3
Aiden-Taylor <magootaylor@gmail.com>
2024-03-05 22:48:59 -0800

    Update senstst.py

--- senstst.py
+++ senstst.py
@@ -10,7 +10,7 @@ while True:
         mag_x, mag_y, mag_z = mmc.getCompass()
         
         #print("X:{0:10.2f}, Y:{1:10.2f}, Z:{2:10.2f}".format(mag_x, mag_y, mag_z))
-        print(lsm.getCurrAzimuth())
+        print(lsm.getCurrAzimuth(25))
         time.sleep(1)
     except KeyboardInterrupt:
         break
