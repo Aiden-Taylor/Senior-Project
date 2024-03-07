3354dfa458f4dd54b5f5d83058c437e4d074bb79
Aiden-Taylor <magootaylor@gmail.com>
2024-03-07 13:58:23 -0800

    Added PWM

---
+++ pwm.py
@@ -0,0 +1,32 @@
+import board
+import pwmio
+import RPi.GPIO as GPIO
+
+
+
+
+class pwm:
+    
+    def __init__(self):
+        self.act_fwd = pwmio.PWMOut(board.D18, frequency=5000, duty_cycle=0)
+        self.act_rev = pwmio.PWMOut(board.D12, frequency=5000, duty_cycle=0)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+
+        GPIO.setup(7, GPIO.OUT)
+        GPIO.setup(8, GPIO.OUT)
+
+    def set_speed(self, spd):
+        if spd > 0:
+            GPIO.out(7, GPIO.HIGH)
+            GPIO.out(8, GPIO.LOW)
+
+            self.act_fwd.duty_cycle = spd
+            self.act_rev.duty_cycle = 0
+
+        else:
+            GPIO.out(7, GPIO.LOW)
+            GPIO.out(8, GPIO.HIGH)
+
+            self.act_fwd.duty_cycle = 0
+            self.act_rev.duty_cycle = -spd
--- senstst.py
+++ senstst.py
@@ -1,16 +1,23 @@
 import gyroobj
 import compass
+import pwm
 import time
 
 lsm = gyroobj.gyroobj()
 mmc = compass.compass()
 
-while True:
-    try:
-        mag_x, mag_y, mag_z = mmc.getCompass()
+
+actuator = pwm.pwm()
+
+actuator.set_speed(100)
+time.sleep(1)
+actuator.set_speed(0)
+# while True:
+#     try:
+#         mag_x, mag_y, mag_z = mmc.getCompass()
         
-        #print("X:{0:10.2f}, Y:{1:10.2f}, Z:{2:10.2f}".format(mag_x, mag_y, mag_z))
-        print(lsm.getCurrAzimuth(25))
-        time.sleep(1)
-    except KeyboardInterrupt:
-        break
+#         #print("X:{0:10.2f}, Y:{1:10.2f}, Z:{2:10.2f}".format(mag_x, mag_y, mag_z))
+#         print(lsm.getCurrAzimuth(25))
+#         time.sleep(1)
+#     except KeyboardInterrupt:
+#         break
