437490d870bdfa71827742995f67341e81e2a54c
Aiden-Taylor <magootaylor@gmail.com>
2024-03-07 14:06:26 -0800

    Update pwm.py

--- pwm.py
+++ pwm.py
@@ -18,15 +18,15 @@ class pwm:
 
     def set_speed(self, spd):
         if spd > 0:
-            GPIO.out(7, GPIO.HIGH)
-            GPIO.out(8, GPIO.LOW)
+            GPIO.OUT(7, 1)
+            GPIO.OUT(8, 0)
 
             self.act_fwd.duty_cycle = spd
             self.act_rev.duty_cycle = 0
 
         else:
-            GPIO.out(7, GPIO.LOW)
-            GPIO.out(8, GPIO.HIGH)
+            GPIO.OUT(7, 0)
+            GPIO.OUT(8, 1)
 
             self.act_fwd.duty_cycle = 0
             self.act_rev.duty_cycle = -spd
