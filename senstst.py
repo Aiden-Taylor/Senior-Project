f798ca1e56ec03ebe9188ea5c3f73312e73c339c
Aiden-Taylor <magootaylor@gmail.com>
2024-03-14 08:21:47 -0700

    Update senstst.py

--- senstst.py
+++ senstst.py
@@ -7,17 +7,17 @@ lsm = gyroobj.gyroobj()
 mmc = compass.compass()
 
 
-actuator = pwm.pwm()
+# actuator = pwm.pwm()
 
-actuator.set_speed(100)
-time.sleep(1)
-actuator.set_speed(0)
-# while True:
-#     try:
-#         mag_x, mag_y, mag_z = mmc.getCompass()
+# actuator.set_speed(100)
+# time.sleep(1)
+# actuator.set_speed(0)
+while True:
+    try:
+        mag_x, mag_y, mag_z = mmc.getCompass()
         
-#         #print("X:{0:10.2f}, Y:{1:10.2f}, Z:{2:10.2f}".format(mag_x, mag_y, mag_z))
-#         print(lsm.getCurrAzimuth(25))
-#         time.sleep(1)
-#     except KeyboardInterrupt:
-#         break
+        #print("X:{0:10.2f}, Y:{1:10.2f}, Z:{2:10.2f}".format(mag_x, mag_y, mag_z))
+        print(lsm.getCurrAzimuth(25))
+        time.sleep(1)
+    except KeyboardInterrupt:
+        break
